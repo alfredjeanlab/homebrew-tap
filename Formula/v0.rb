e24a0f18656f35c8c6819249ad6a17bcef2d7125
Kevin Stenerson <kevin@stenerson.me>
2026-01-30 15:18:43 -0500

    v0 0.3.3

--- Formula/v0.rb
+++ Formula/v0.rb
@@ -1,8 +1,8 @@
 class V0 < Formula
   desc "A tool to ease you in to multi-agent vibe coding"
   homepage "https://github.com/alfredjeanlab/v0"
-  url "https://github.com/alfredjeanlab/v0/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "4d05b5d662e1f79f0dd8bec04a98ae282291c754fad37cf4b86edb2c41ea678a"
+  url "https://github.com/alfredjeanlab/v0/archive/refs/tags/v0.3.3.tar.gz"
+  sha256 "5a052a2efb546d7486e9afa1afe7453986d0c2b9bfee93e388918df328d629be"
   license "MIT"
 
   depends_on "alfredjeanlab/tap/wok"
@@ -14,6 +14,11 @@ class V0 < Formula
   def install
     bin.install Dir["bin/*"]
     lib.install Dir["lib/*"]
+    # Install each package's lib directory preserving package names
+    Dir["packages/*"].each do |pkg|
+      next unless File.directory?("#{pkg}/lib")
+      (prefix/pkg).install "#{pkg}/lib"
+    end
   end
 
   test do
